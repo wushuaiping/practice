74945d31054e34caa3b328625f1d765aacb4ce0e
Spring <spwu@creams.io>
2019-07-05 09:46:01 +0800

    init

---
+++ evertools/src/main/java/io/wooo/evertools/EvertoolsApplication.java
@@ -0,0 +1,13 @@
+package io.wooo.evertools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EvertoolsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EvertoolsApplication.class, args);
+	}
+
+}
