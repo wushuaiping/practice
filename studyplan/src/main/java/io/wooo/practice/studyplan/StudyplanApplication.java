78c47cb606ee87489147057d0519543ae554f3c5
wushuaiping <603413965@qq.com>
2019-07-11 22:34:36 +0800

    add

---
+++ studyplan/src/main/java/io/wooo/practice/studyplan/StudyplanApplication.java
@@ -0,0 +1,13 @@
+package io.wooo.practice.studyplan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyplanApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudyplanApplication.class, args);
+    }
+
+}
